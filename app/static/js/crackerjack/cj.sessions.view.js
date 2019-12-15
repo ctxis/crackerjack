4f0e35fc6ee43103f2ed202c806ca535c0081780
Pavel Tsakalidis <p@vel.gr>
2019-12-15 23:13:55 +0000

    Add confirmation for action-reset

--- app/static/js/crackerjack/cj.sessions.view.js
+++ app/static/js/crackerjack/cj.sessions.view.js
@@ -1,6 +1,12 @@
 var CJ_SessionsView = {
     init: function() {
         $('.form-action').click(function() {
+            // Add an extra check for the 'reset' link.
+            if ($(this).data('action') == 'reset') {
+                if (!confirm("This will kill the current screen session. You should only run this if you cannot get hashcat to start.\n\nAre you sure you want to do this?")) {
+                    return false;
+                }
+            }
             $('#action').val($(this).data('action'));
             $('#form-action').submit();
             return false;
