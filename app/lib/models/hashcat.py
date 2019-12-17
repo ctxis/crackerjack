54f76a21115dd835d1720f241df3a59d153a435a
Pavel Tsakalidis <p@vel.gr>
2019-12-17 16:25:52 +0000

    Updated sql fields to nullable due to upgrade errors

--- app/lib/models/hashcat.py
+++ app/lib/models/hashcat.py
@@ -5,17 +5,18 @@ import datetime
 class HashcatModel(db.Model):
     __tablename__ = 'hashcat'
     id = db.Column(db.Integer, primary_key=True)
-    session_id = db.Column(db.Integer, default=0, index=True, nullable=False)
-    mode = db.Column(db.Integer, default=0, index=True, nullable=False)
-    hashtype = db.Column(db.String, default='', index=True, nullable=False)
-    wordlist = db.Column(db.String, default='', index=True, nullable=False)
-    rule = db.Column(db.String, default='', index=True, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+    session_id = db.Column(db.Integer, default=0, index=True, nullable=True)
+    mode = db.Column(db.Integer, default=0, index=True, nullable=True)
+    hashtype = db.Column(db.String, default='', index=True, nullable=True)
+    wordlist = db.Column(db.String, default='', index=True, nullable=True)
+    rule = db.Column(db.String, default='', index=True, nullable=True)
+    mask = db.Column(db.String, default='', index=True, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=True, default=datetime.datetime.now())
 
 
 class UsedWordlistModel(db.Model):
     __tablename__ = 'used_wordlists'
     id = db.Column(db.Integer, primary_key=True)
-    session_id = db.Column(db.Integer, default=0, index=True, nullable=False)
-    wordlist = db.Column(db.String, default='', index=True, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+    session_id = db.Column(db.Integer, default=0, index=True, nullable=True)
+    wordlist = db.Column(db.String, default='', index=True, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=True, default=datetime.datetime.now())
--- app/lib/models/sessions.py
+++ app/lib/models/sessions.py
@@ -5,8 +5,8 @@ import datetime
 class SessionModel(db.Model):
     __tablename__ = 'sessions'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, default=0, index=True, nullable=False)
-    name = db.Column(db.String, default='', index=True, nullable=False)
-    screen_name = db.Column(db.String, default='', index=True, nullable=False)
-    active = db.Column(db.Boolean, default=False, index=True)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now())
+    user_id = db.Column(db.Integer, default=0, index=True, nullable=True)
+    name = db.Column(db.String, default='', index=True, nullable=True)
+    screen_name = db.Column(db.String, default='', index=True, nullable=True)
+    active = db.Column(db.Boolean, default=False, index=True, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=True, default=datetime.datetime.now())
