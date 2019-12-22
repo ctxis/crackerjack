fa635e03b6d12be75db2a4d9ff296e8a47b90de6
Pavel Tsakalidis <p@vel.gr>
2019-12-22 15:48:07 +0000

    Added name column to ApiKey table

--- app/lib/models/api.py
+++ app/lib/models/api.py
@@ -5,5 +5,6 @@ class ApiKeys(db.Model):
     __tablename__ = 'api_keys'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, default=0, index=True)
+    name = db.Column(db.String, default='', nullable=True)
     apikey = db.Column(db.String, default='', nullable=True)
     enabled = db.Column(db.Boolean, default=False, index=True, nullable=True)
