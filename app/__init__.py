3ae085971903188a7e40f0ae1b6967e0df0d8798
Pavel Tsakalidis <p@vel.gr>
2019-12-26 22:27:42 +0000

    Implement a 404 page

--- app/__init__.py
+++ app/__init__.py
@@ -82,6 +82,10 @@ def create_app(config_class=None):
     def internal_error(error):
         return render_template('errors/500.html'), 500
 
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html'), 404
+
     return app
 
 
