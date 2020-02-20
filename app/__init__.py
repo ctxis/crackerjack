1154daae0a846ec8b9bde58f7b1702fa1c17975c
Pavel Tsakalidis <p@vel.gr>
2020-02-20 17:53:58 +0000

    INTERNAL-40 / Move instance into the ./data folder

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,10 @@ crontab = Crontab()
 
 
 def create_app(config_class=None):
-    app = Flask(__name__, instance_relative_config=True)
+    # Make sure the instance path is within the ./data folder.
+    data_instance_path = os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data', 'instance'))
+
+    app = Flask(__name__, instance_path=data_instance_path, instance_relative_config=True)
 
     try:
         os.makedirs(app.instance_path)
