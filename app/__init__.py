be82c8640640ab385c3f10f4ce6bb2a0f36ac811
Pavel Tsakalidis <p@vel.gr>
2020-02-20 16:32:21 +0000

    Added debug trace to errors

--- app/__init__.py
+++ app/__init__.py
@@ -109,11 +109,11 @@ def create_app(config_class=None):
 
     @app.errorhandler(500)
     def internal_error(error):
-        return render_template('errors/500.html'), 500
+        return render_template('errors/500.html', error=error), 500
 
     @app.errorhandler(404)
     def page_not_found(error):
-        return render_template('errors/404.html'), 404
+        return render_template('errors/404.html', error=error), 404
 
     @crontab.job(minute="*/5")
     def cron():
